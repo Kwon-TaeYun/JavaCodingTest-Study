277d69f28fe6f044ffd9fcda89415c83bf05018c
gwon-taeyun <taeyun1118@naver.com>
2025-01-24 15:50:30 +0900

    first commit

---
+++ src/day01/배열제어하기/ControllSort.java
@@ -0,0 +1,47 @@
+package day01.배열제어하기;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static java.util.Arrays.*;
+
+public class ControllSort {
+    public static void main(String[] args) {
+        int[] input = {4,2,2,1,3,4};
+        Integer[] chk = new Integer[input.length];
+        int index = 1; //chk의 인덱스
+
+        //1. 일단 오름차순 정렬
+        sort(input);
+
+        //2. 정렬된 input의 첫번째 인덱스 chk에 넣기
+       chk[0] = input[0];
+
+        //3.
+        for(int i = 1; i < input.length; i++){
+            if(chk[index - 1] != input[i]){
+                chk[index] = input[i];
+                index++;
+            }
+        }
+
+        //4. 마지막부터 비교하여 null이 아니면 최종 output에 집어넣어주기
+        Integer[] output = new Integer[index];
+        int outdex = 0;
+        for(int o = input.length-1; o >= 0; o--){
+            if(chk[o] != null){
+                output[outdex] = chk[o];
+                outdex++;
+            }
+        }
+
+
+
+        //System.out.println(Arrays.toString(input));
+        System.out.println(index);
+        System.out.println(Arrays.toString(chk));
+        System.out.println(Arrays.toString(output));
+
+    }
+}
+
+
