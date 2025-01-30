2fae62f4807d61e67fab95eeded1c872c6e12719
gwon-taeyun <taeyun1118@naver.com>
2025-01-30 12:50:07 +0900

    first commit

--- src/day01/배열자르기/CutSort.java
+++ src/day01/배열자르기/CutSort.java
@@ -1,7 +1,24 @@
 package day01.배열자르기;
 
-public class CutSort {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class CutSort {
+    public static int[] solution(int n, long left, long right) {
+        int size = (int)(right - left + 1);
+        int[] result = new int[size];
+
+        for(int i = 0; i < size; i++){
+            long index = left + i;
+            int row = (int)(index / n);
+            int col = (int)(index % n);
+            result[i] = Math.max(row+1, col+1);
+
+        }
+
+        return result;
+    }
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(3,2,5)));
+        System.out.println(Arrays.toString(solution(4,7,14)));
     }
 }
